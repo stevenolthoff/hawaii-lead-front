daf7190c8ba9c3bc5e9807b5a390d00a0bff8309
Steven Olthoff <steven@axiomdatascience.com>
2023-10-20 13:57:18 -0400

    update fixture codes

--- src/Services/FixtureLabel.ts
+++ src/Services/FixtureLabel.ts
@@ -3,15 +3,19 @@ export function getFixtureLabel (fixtureType: string) {
   if (fixtureType === 'BF') {
     return 'Bathroom'
   } else if (fixtureType === 'CF') {
-    return 'Class'
+    return 'Classroom'
   } else if (fixtureType === 'DF') {
     return 'Drinking'
   } else if (fixtureType === 'KF') {
     return 'Kitchen'
   } else if (fixtureType === 'KPF') {
-    return 'Kitchen Potable'
+    return 'Kitchen Pot'
   } else if (fixtureType === 'NS') {
-    return 'NS'
+    return 'Nurse Office'
+  } else if (fixtureType === 'WC') {
+    return 'Water Cooler'
+  } else if (fixtureType === 'WBF') {
+    return 'Water Bottle Filler'
   } else {
     return fixtureType
   }
